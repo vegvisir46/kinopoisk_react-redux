8ffa727f154f598ea98535567ca2fa5725b1d4b8
Alexander <alexandr.rostow@gmail.com>
2022-09-06 13:41:13 +0700

    deploy to vercel

--- src/apiData/token.jsx
+++ src/apiData/token.jsx
@@ -1,3 +1,5 @@
-const token = 'f62af618-6ca3-446c-8931-2f865c060cc9';
+// const token = 'f62af618-6ca3-446c-8931-2f865c060cc9'; // main
+// const token = '3a8203f5-b9e1-4d59-b0ed-42517ef90d44';
+const token = '9260945d-a5dd-4007-96f9-d0711b675739';
 
 export default token;
